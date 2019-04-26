f1839dcac01927632fa41d8932bccd7ac80e799d
heipacker <heipackermail@gmail.com>
2019-04-27 03:33:32 +0800

    new: 支持多个bp

--- claimer.js
+++ claimer.js
@@ -2,70 +2,70 @@ const Eos = require('eosjs');
 const config = require('./config.json');
 const httpEndPoint = config.httpEndPoint;
 const chainId = config.chainId;
-const wif = config.wif;
-const producerName = config.producerName;
-const permission = config.permission;
-
-var eos = Eos({
-    httpEndpoint: httpEndPoint, chainId: chainId,
-    keyProvider: wif
-});
-
 
 cacheRewards();
 //try every 10 min
 setInterval(cacheRewards, 10 * 60 * 1000 + 5000);
+
 //////////////////////////
 function cacheRewards() {
-    Promise.all([getGlobal(), getProducer(producerName)]).then(([global, producer]) => {
-        let bpay = (global.perblock_bucket * producer.unpaid_blocks) / global.total_unpaid_blocks / 10000;
-        let vpay = (global.pervote_bucket * producer.total_votes) / (1 * global.total_producer_vote_weight) / 10000;
-        if (vpay < 100) {
-            vpay = 0;
-        }
-        let next_claim_time = 1 * producer.last_claim_time / 1000 + 24 * 60 * 60 * 1000;
-        if (next_claim_time > Date.now()) {
-            return 0;
-        }
-        return bpay + vpay;
-    }, errs => {
-        console.error(errs);
-        //retry
-        cacheRewards();
-    }).then(rewards => {
-        console.log("current rewards:", rewards);
-        if (rewards > 0) {
-            eos.transaction({
-                // ...headers,
-                actions: [
-                    {
-                        account: 'enumivo',
-                        name: 'claimrewards',
-                        authorization: [{
-                            actor: producerName,
-                            permission: permission
-                        }],
-                        data: {
-                            owner: producerName
+    const bps = config.bps;
+    for (var i = 0; i < bps.length; ++i) {
+        let bp = bps[i];
+        const wif = bp.wif;
+        const producerName = bp.producerName;
+        const permission = bp.permission;
+        var eos = Eos({
+            httpEndpoint: httpEndPoint, chainId: chainId,
+            keyProvider: wif
+        });
+
+        Promise.all([getGlobal(eos), getProducer(eos, producerName)]).then(([global, producer]) => {
+            let bpay = (global.perblock_bucket * producer.unpaid_blocks) / global.total_unpaid_blocks / 10000;
+            let vpay = (global.pervote_bucket * producer.total_votes) / (1 * global.total_producer_vote_weight) / 10000;
+            if (vpay < 100) {
+                vpay = 0;
+            }
+            let next_claim_time = 1 * producer.last_claim_time / 1000 + 24 * 60 * 60 * 1000;
+            if (next_claim_time > Date.now()) {
+                return 0;
+            }
+            return bpay + vpay;
+        }, errs => {
+            console.error(errs);
+        }).then(rewards => {
+            console.log("current rewards:", rewards);
+            if (rewards > 0) {
+                eos.transaction({
+                    // ...headers,
+                    actions: [
+                        {
+                            account: 'eosio',
+                            name: 'claimrewards',
+                            authorization: [{
+                                actor: producerName,
+                                permission: permission
+                            }],
+                            data: {
+                                owner: producerName
+                            }
                         }
-                    }
-                ]
-            }).then(res => {
-                console.log(res);
-            }, err => {
-                console.error(err);
-                //retry
-                cacheRewards();
-            });
-        }
-    });
+                    ]
+                }).then(res => {
+                    console.log(res);
+                }, err => {
+                    console.error(err);
+                });
+            }
+        });
+    }
 }
 
-function getGlobal() {
+function getGlobal(eos) {
     return new Promise((resolve, reject) => {
         eos.getTableRows({
-            "scope": "enumivo",
-            "code": "enumivo",
+            "scope": "eosio",
+            "code": "eosio",
             "table": "global",
             "json": true
         }).then(res => {
@@ -77,11 +77,11 @@ function getGlobal() {
     });
 }
 
-function getProducer(name) {
+function getProducer(eos, name) {
     return new Promise((resolve, reject) => {
         eos.getTableRows({
-            "scope": "enumivo",
-            "code": "enumivo",
+            "scope": "eosio",
+            "code": "eosio",
             "table": "producers",
             "lower_bound": name,
             "limit": 1,
